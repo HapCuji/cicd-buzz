e69f5fa6e89db2645d25061094764d93a4edc465
HapCuji <hapcuji@gmail.com>
2018-11-14 02:19:09 +0300

    +2

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -2,11 +2,5 @@
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-if [$TRAVIS_BRANCH == "master"];	then
-		TAG = "latest"
-	else
-		TAG = "$TRAVIS_BRANCH"
-fi 
-
-docker build -f Dockerfile -t $DOCKER_USER/cicd-buzz:$TAG . # "." - empty arg
+docker build -f Dockerfile -t $DOCKER_USER/cicd-buzz:$TRAVIS_BRANCH . # "." - empty arg
 docker push $DOCKER_USER/cicd-buzz
--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 #heroku plugins:install heroku-container-registry
-heroku login
 heroku plugins:install @heroku-cli/plugin-container-registry
-#docker login -u $DOCKER_EMAIL --password=$HEROKU_API_KEY registry.heroku.com 
 heroku container:login
+#docker login --username=_ --password==$HEROKU_API_KEY registry.heroku.com 
 heroku container:push web --app $HEROKU_APP_NAME
