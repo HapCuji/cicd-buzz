8598c03fc8e48be7b19da93e1069cef6e3f78110
HapCuji <hapcuji@gmail.com>
2018-11-14 00:51:38 +0300

    new trav step 8

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,11 @@
 #!bin/sh
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if {"$TRAVIS_BRANCH" = "master"}; then
 	TAG = "latest"
 else
 	TAG = "$TRAVIS_BRANCH"
 	
 fi 
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG . # "." - empty arg
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG . # "." - empty arg
+docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
