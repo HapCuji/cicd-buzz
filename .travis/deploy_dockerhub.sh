429d921fa961951c1b257492fa502a1ef37fa99a
HapCuji <hapcuji@gmail.com>
2018-11-14 01:27:16 +0300

    .travis

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,12 @@
 #!bin/sh
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-if {"$TRAVIS_BRANCH" = "master"}; then
-	TAG = "latest"
-else
-	TAG = "$TRAVIS_BRANCH"
-	
+if ["$TRAVIS_BRANCH" = "master"]
+	then
+		TAG = "latest"
+	else
+		TAG = "$TRAVIS_BRANCH"
 fi 
-docker build -f Dockerfile -t $DOCKER_USER/$TRAVIS_REPO_SLUG:$TAG . # "." - empty arg
-docker push $DOCKER_USER/$TRAVIS_REPO_SLUG
+
+docker build -f Dockerfile -t $DOCKER_USER/cicd-buzz:$TAG . # "." - empty arg
+docker push $DOCKER_USER/cicd-buzz
